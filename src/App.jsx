02e508ca78eb25b52afb8a160b9126463034661f
GOKUL-A-N <gokulcricketer141103@gmail.com>
2024-07-31 15:21:02 +0530

    Feat : initiall Commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+
+
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [promptResponses, setpromptResponses] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyCjZHjL5x-6tnaunWkBqpitv_1rDf4u1OA"
+// add your api key here
+  );
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const getResponseForGivenPrompt = async () => {
+    try {
+      setLoading(true)
+      const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+      const result = await model.generateContent(inputValue);
+      setInputValue('')
+      const response = result.response;
+      const text = response.text();
+      console.log(text)
+      setpromptResponses([...promptResponses,text]);
+  
+      setLoading(false)
+    }
+    catch (error) {
+      console.log(error)
+      console.log("Something Went Wrong");
+      setLoading(false)
+    }
+  }
+    ;
+
+  return (
+    <div className="container">
+    <div className="row">
+      <div className="col">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Ask Me Something You Want"
+          className="form-control"
+        />
+      </div>
+      <div className="col-auto">
+        <button onClick={getResponseForGivenPrompt} className="btn btn-primary">Send</button>
+      </div>
+    </div>
+    {loading ? (
+      <div className="text-center mt-3">
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+       // This message is shown while your answer to your prompt is being generated
+        </div>
+      </div>
+    ) : (
+      promptResponses.map((promptResponse, index) => (
+        <div key={index} >
+          <div className={`response-text ${index === promptResponses.length - 1 ? 'fw-bold' : ''}`}>{promptResponse}</div>
+     //the latest response shown in bold letters
+        </div>
+      ))
+    )}
+  </div>
+  
+  );
+
+}
+export default App;
